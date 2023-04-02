fa6a58b56a039d88225507db1d4d3edca6dc920c
hsh5206 <hyunhsh5206@gmail.com>
2023-04-03 03:30:14 +0900

    Setup

---
+++ Maze/Player.cpp
@@ -0,0 +1,65 @@
+#include "pch.h"
+#include "Player.h"
+#include "Board.h"
+
+void Player::Init(Board* board)
+{
+	_pos = board->GetEnterPos();
+	_board = board;
+	
+	Pos pos = _pos;
+	
+	_path.clear();
+	_path.push_back(pos);
+
+	Pos front[4] =
+	{
+		Pos{-1, 0}, // UP
+		Pos{0, -1}, // LEFT
+		Pos{1, 0}, // DOWN
+		Pos{0, 1}, // RIGHT
+	};
+
+	// 목적지 도착전에 실행
+	Pos dest = board->GetExitPos();
+	while (pos != dest)
+	{
+		// 오른손 법칙
+		int32 newDir = (_dir - 1 + DIR_COUNT) % DIR_COUNT;
+		if (CanGo(pos + front[newDir]))
+		{
+			_dir = newDir;
+			pos += front[_dir];
+			_path.push_back(pos);
+		}
+		else if (CanGo(pos+front[_dir]))
+		{
+			pos += front[_dir];
+			_path.push_back(pos);
+		}
+		else
+		{
+			_dir = (_dir + 1) % DIR_COUNT;
+		}
+	}
+}
+
+void Player::Update(uint64 deltaTick)
+{
+	if (_pathIndex >= _path.size())
+		return;
+
+	_sumTick += deltaTick;
+	if (_sumTick >= MOVE_TICK)
+	{
+		_sumTick = 0;
+		_pos = _path[_pathIndex];
+		_pathIndex++;
+	}
+}
+
+bool Player::CanGo(Pos pos)
+{
+	TileType tileType = _board->GetTileType(pos);
+	return tileType == TileType::EMPTY;
+}
